0f0d337678c27c9e3c84e14ba5664629210befd9
Shreya Swaroop <shreyaswrp@gmail.com>
2020-08-26 17:31:02 +0530

    Added to print a welcome message

---
+++ src/main/java/co/CricketLeagueAnalyzer/CricketLeagueAnalyzer.java
@@ -0,0 +1,8 @@
+package co.CricketLeagueAnalyzer;
+
+public class CricketLeagueAnalyzer {
+
+    public static void main(String args[]){
+        System.out.println("Welcome to Cricket League Analyzer Program");
+    }
+}
